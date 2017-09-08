0da5a42930fa825a0dd1a44dbc54c3cf7338238c
musyoku <musyoku@users.noreply.github.com>
2017-09-08 16:40:44 +0900

    :v:

--- run/ctc/cnn/model.py
+++ run/ctc/cnn/model.py
@@ -123,7 +123,7 @@ def build_model(vocab_size, ndim_audio_features=3, ndim_h=128, ndim_dense=320, k
 	if architecture == "zhang+fc_relu":
 		# first layer
 		model.layer(
-			nn.Convolution2D(ndim_audio_features, ndim_h * 2, kernel_size, stride=1, pad=(0, kernel_size[1] - 1), initialW=initializers.Normal(math.sqrt(wgain / ndim_audio_features / kernel_size[0] / kernel_size[1])), weightnorm=weightnorm),
+			nn.Convolution2D(ndim_audio_features, ndim_h * 2, kernel_size, stride=1, pad=(0, kernel_size[1] - 1), weightnorm=weightnorm),
 			lambda x: x[..., :-pad],
 			nn.Maxout(2),
 			nn.Dropout(dropout),
@@ -138,7 +138,7 @@ def build_model(vocab_size, ndim_audio_features=3, ndim_h=128, ndim_dense=320, k
 
 		for in_channels, out_channels in in_out:
 			model.layer(
-				nn.Convolution2D(in_channels, out_channels, kernel_size, stride=1, pad=(1, kernel_size[1] - 1), initialW=initializers.Normal(math.sqrt(wgain / ndim_h / kernel_size[0] / kernel_size[1])), weightnorm=weightnorm),
+				nn.Convolution2D(in_channels, out_channels, kernel_size, stride=1, pad=(1, kernel_size[1] - 1), weightnorm=weightnorm),
 				lambda x: x[..., :-pad],
 				nn.Maxout(2),
 				nn.Dropout(dropout),
@@ -148,7 +148,7 @@ def build_model(vocab_size, ndim_audio_features=3, ndim_h=128, ndim_dense=320, k
 			in_out = [(ndim_h * 2, ndim_h * 4)] * num_conv_layers_narrow
 			for in_channels, out_channels in in_out:
 				model.layer(
-					nn.Convolution2D(in_channels, out_channels, kernel_size, stride=1, pad=(1, kernel_size[1] - 1), initialW=initializers.Normal(math.sqrt(wgain / ndim_h / kernel_size[0] / kernel_size[1])), weightnorm=weightnorm),
+					nn.Convolution2D(in_channels, out_channels, kernel_size, stride=1, pad=(1, kernel_size[1] - 1), weightnorm=weightnorm),
 					lambda x: x[..., :-pad],
 					nn.Maxout(2),
 					nn.Dropout(dropout),
